110a60703ab4a663852fd6fef17c72f5ed671497
Aliyoub <abisocial@hotmail.com>
2019-03-20 23:18:25 +0100

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,10 +2,10 @@ export const environment = {
   production: false
 };
 export const firebaseConfig = {
-    apiKey: 'AIzaSyBAb8GFJhEajE2Gac31IhekUpILSXmACdA',
-    authDomain: 'entrevoisins-f78ce.firebaseapp.com',
-    databaseURL: 'https://entrevoisins-f78ce.firebaseio.com',
-    projectId: 'entrevoisins-f78ce',
-    storageBucket: 'entrevoisins-f78ce.appspot.com',
-    messagingSenderId: '972898712263'
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
+    storageBucket: '',
+    messagingSenderId: ''
 };
